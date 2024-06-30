7896d5924b1438ba199e26e5f21ba944acfc6376
jbcodeforce <jerome.boyer@gmail.com>
2024-06-30 09:14:22 -0700

    update the pydantic

--- IBU-insurance-demo/ibu_backend/src/openapi/create_pydantic_from_openapi.sh
+++ IBU-insurance-demo/ibu_backend/src/openapi/create_pydantic_from_openapi.sh
@@ -1,1 +1,1 @@
-datamodel-codegen  --input datamgr_openapi.yaml --input-file-type openapi --output ../ibu/itg/ds/ComplaintHandling_generated_model.py
+datamodel-codegen  --input ComplaintHandlingNextBestActionDecisionService.yaml --input-file-type openapi --output ../ibu/itg/ds/ComplaintHandling_generated_model.py
