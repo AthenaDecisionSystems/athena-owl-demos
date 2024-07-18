b08fc2adfaf2e3202c256ceb6a06df9ac43ad4c2
jbcodeforce <jerome.boyer@gmail.com>
2024-07-17 22:10:58 -0700

    add validation of the configuration

--- IBM-MiniLoan-demo/ibu_backend/tests/ut/test_read_prompts.py
+++ IBM-MiniLoan-demo/ibu_backend/tests/ut/test_read_prompts.py
@@ -4,16 +4,18 @@ module_path = "./src"
 sys.path.append(os.path.abspath(module_path))
 os.environ["CONFIG_FILE"] = "./tests/ut/config/config.yaml"
 
-from athena.llm.prompts.prompt_mgr import get_prompt_manager, OwlPromptEntity
-
+from athena.llm.prompts.prompt_mgr import get_prompt_manager
+from langchain_core.prompts.chat import ChatPromptTemplate
 class TestPrompts(unittest.TestCase):
     def test_build_prompt_from_prompt_entity(self):
         print("\n\n --- test_build_prompt_instance_from_prompt_entity")
         p_mgr=get_prompt_manager()
         prompt = p_mgr.build_prompt("ibu_loan_prompt","en")
-        print(type(prompt))
-        print(prompt) 
-
+        assert isinstance(prompt, ChatPromptTemplate)
+        
+        assert prompt.input_variables[0] == "input"
+        assert "bank assistant" in prompt.messages[0].prompt.template
+      
 
 
 if __name__ == '__main__':
