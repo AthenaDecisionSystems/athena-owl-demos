c29e89ef8f46f06363c763aa45f8ddec8762f0fc
Harley Davis <harley@athenadecisions.com>
2024-11-21 09:10:58 +0100

    latest

--- IBU-insurance-demo/ibu_backend/src/start_backend.sh
+++ IBU-insurance-demo/ibu_backend/src/start_backend.sh
@@ -1,4 +1,5 @@
 export CONFIG_FILE=./config/config_dev_outside_docker.yaml
 export OWL_CLIENTS=[http://localhost:3000,http://localhost:3001]
 
-uvicorn athena.main:app --host 0.0.0.0 --port 8002 --reload 
+#uvicorn athena.main:app --host 0.0.0.0 --port 8002 --reload
+uvicorn athena.main:app --host 0.0.0.0 --port 8002
